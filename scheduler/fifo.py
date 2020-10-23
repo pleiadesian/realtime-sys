df5ce45026b391bed46ed06ff9de45c62013dbf8
sinkinben <sinkinben@outlook.com>
2020-10-23 09:46:42 +0800

    fix: fix bug of fifo schedule

--- scheduler/fifo.py
+++ scheduler/fifo.py
@@ -3,12 +3,14 @@ from scheduler.abstract_class import Scheduler
 class FIFOScheduler(Scheduler):
     def schedule(self, workloads):
         seq = []
-        for i in range(0, len(workloads)):
-            task = workloads[i]
+        # add an order number for each task
+        tasks = zip(list(range(0, len(workloads))), workloads)
+        # sort by start time
+        tasks = sorted(tasks, key=lambda x: x[1][0])
+        for i in range(0, len(tasks)):
+            task_order, task = tasks[i][0], tasks[i][1]
             compute_time = task[2]
-            while compute_time != 0:
-                compute_time -= 1
-                seq.append(i)
+            seq += [task_order] * compute_time
         return seq
 
 

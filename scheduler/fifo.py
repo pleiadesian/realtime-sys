0b1efa5df1785ff70da0ab2212ebdedf0365e357
sinkinben <sinkinben@outlook.com>
2020-10-23 16:38:35 +0800

    feat: sort workloads by start time before shceduling in fifo

--- scheduler/fifo.py
+++ scheduler/fifo.py
@@ -7,6 +7,9 @@ class FIFOScheduler(Scheduler):
         tasks = zip(list(range(0, len(workloads))), workloads)
         # sort by start time
         tasks = sorted(tasks, key=lambda x: x[1][0])
+        # maybe an idle before 1st task arrived
+        begin_time = tasks[0][1][0]
+        seq += begin_time * [-1]
         for i in range(0, len(tasks)):
             task_order, task = tasks[i][0], tasks[i][1]
             compute_time = task[2]
